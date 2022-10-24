e10060f533006415ca7e33adc5a2e2cce418880c
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2022-10-24 18:38:38 -0500

    real time trade info

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ const finnhub = require('finnhub');
 const api_key = finnhub.ApiClient.instance.authentications['api_key'];
 api_key.apiKey = process.env.API_KEY;
 const finnhubClient = new finnhub.DefaultApi();
+const WebSocket = require('ws');
+const socket = new WebSocket('wss://ws.finnhub.io?token=' + process.env.API_KEY);
 const app = (0, express_1.default)();
 const PORT = 3000;
 // Handling GET / Request
@@ -17,8 +19,25 @@ app.get('/', (req, res) => {
 });
 app.get('/general', (req, res) => {
     finnhubClient.marketNews("general", {}, (error, data, response) => {
+        //res.send(data[0]["headline"] + "<br>" + data[0]["summary"])
         res.send(data);
     });
+});
+app.get('/realtimeTrades', (req, res) => {
+    // Connection opened -> Subscribe
+    socket.addEventListener('open', function (event) {
+        socket.send(JSON.stringify({ 'type': 'subscribe', 'symbol': 'AAPL' }));
+        socket.send(JSON.stringify({ 'type': 'subscribe', 'symbol': 'BINANCE:BTCUSDT' }));
+        socket.send(JSON.stringify({ 'type': 'subscribe', 'symbol': 'IC MARKETS:1' }));
+    });
+    // Listen for messages
+    socket.addEventListener('message', function (event) {
+        res.send(event.data);
+    });
+    // Unsubscribe
+    var unsubscribe = function (symbol) {
+        socket.send(JSON.stringify({ 'type': 'unsubscribe', 'symbol': symbol }));
+    };
 });
 // Server setup
 app.listen(PORT, () => {
--- backend/server.ts
+++ backend/server.ts
@@ -7,6 +7,10 @@ const api_key = finnhub.ApiClient.instance.authentications['api_key'];
 api_key.apiKey = process.env.API_KEY
 const finnhubClient = new finnhub.DefaultApi()
 
+const WebSocket = require('ws');
+const socket = new WebSocket('wss://ws.finnhub.io?token='+process.env.API_KEY);
+
+
   
 const app = express();
 const PORT:Number=3000;
@@ -18,8 +22,29 @@ app.get('/', (req, res) => {
 
 app.get('/general', (req, res) => {
     finnhubClient.marketNews("general", {}, (error: any, data: any, response: any) => {
+        //res.send(data[0]["headline"] + "<br>" + data[0]["summary"])
         res.send(data)
       });
+}) 
+
+
+app.get('/realtimeTrades', (req, res) => {
+    // Connection opened -> Subscribe
+    socket.addEventListener('open', function (event: any) {
+        socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'AAPL'}))
+        socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'BINANCE:BTCUSDT'}))
+        socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'IC MARKETS:1'}))
+    });
+
+    // Listen for messages
+    socket.addEventListener('message', function (event: { data: any; }) {
+        res.send(event.data);
+    });
+
+    // Unsubscribe
+    var unsubscribe = function(symbol: any) {
+        socket.send(JSON.stringify({'type':'unsubscribe','symbol': symbol}))
+    }
 }) 
   
 // Server setup
