897ccaaaf1b2d3a0435c81c0eaf3aa71004cfc25
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2022-10-25 18:36:12 -0500

    background color gradient

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import NewsFeed from "./newsfeed.js"
 
 function App() {
+  document.body.style.backgroundImage = "linear-gradient(to bottom right, #413F42, #7F8487)"
   return (
     <div>
       <NewsFeed></NewsFeed>
