d45d791c5caad81bd5438b4f74acdae02d226145
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2022-10-26 20:35:48 -0500

    navbar style update

--- frontend/src/topNavBar.js
+++ frontend/src/topNavBar.js
@@ -4,33 +4,35 @@ import {Link} from 'react-router-dom';
 function TopNavBar() {
  const mainStyle = {
     width: "100%",
-    backgroundColor: "#413F42",
+    backgroundColor: "#343231",
     paddingBottom: "1em",
     whiteSpace: "pre-line",
     marginBottom: "0.5em",
     marignColor: "white",
+    position: "fixed",
+    top: "0",
  }
 
  function alterColor1(){
   document.getElementById("quantitative").style.backgroundColor="lightgreen";
-  document.getElementById("qualitative").style.backgroundColor="grey";
-  document.getElementById("recomendations").style.backgroundColor="grey";
+  document.getElementById("qualitative").style.backgroundColor="red";
+  document.getElementById("recomendations").style.backgroundColor="red";
  }
  function alterColor2(){
-  document.getElementById("quantitative").style.backgroundColor="grey";
+  document.getElementById("quantitative").style.backgroundColor="red";
   document.getElementById("qualitative").style.backgroundColor="lightgreen";
-  document.getElementById("recomendations").style.backgroundColor="grey";
+  document.getElementById("recomendations").style.backgroundColor="red";
  }
  function alterColor3(){
-  document.getElementById("quantitative").style.backgroundColor="grey";
-  document.getElementById("qualitative").style.backgroundColor="grey";
+  document.getElementById("quantitative").style.backgroundColor="red";
+  document.getElementById("qualitative").style.backgroundColor="red";
   document.getElementById("recomendations").style.backgroundColor="lightgreen";
  }
   return (
     <div style={mainStyle}>
-        <Link to="/news"  onClick={alterColor1}><center><img id="quantitative" src="./icons/quantitative.png" style={{backgroundColor: "grey", display: "content"}}></img></center></Link>
-        <Link to="/stockPrices"  onClick={alterColor2}><img id="qualitative" src="./icons/qualitative.png" style={{position: "absolute", left: "0", top: "0", backgroundColor: "grey"}}></img></Link>
-        <Link to="/recomendations"  onClick={alterColor3}><img id="recomendations" src="./icons/recomendation.png" style={{position: "absolute", right: "0", top:"0", backgroundColor: "grey"}}></img></Link>
+        <Link to="/news"  onClick={alterColor1}><center><img id="quantitative" src="./icons/quantitative.png" style={{backgroundColor: "red", display: "content", position: "relative", top: "0.5em"}}></img></center></Link>
+        <Link to="/stockPrices"  onClick={alterColor2}><img id="qualitative" src="./icons/qualitative.png" style={{position: "absolute", left: "1.5em", top: "0.5em", backgroundColor: "red"}}></img></Link>
+        <Link to="/recomendations"  onClick={alterColor3}><img id="recomendations" src="./icons/recomendation.png" style={{position: "absolute", right: "1.5em", top:"0.5em", backgroundColor: "red"}}></img></Link>
     </div>
   );
 }
